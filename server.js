df81b3a6e9fd1831ec3db67be714e3272f81102d
kike <kike@lightnotes.es>
2020-05-20 19:34:06 +0200

    p5.js example added

--- server.js
+++ server.js
@@ -4,28 +4,29 @@ const express = require('express');
 const { Server } = require('ws');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const INDEX = '/index2.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .use(express.static('public'))
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      client.send(data);
+    console.log('Client connected');
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            client.send(data);
+        });
     });
-  });
 
 
-  ws.on('close', () => console.log('Client disconnected'));
+    ws.on('close', () => console.log('Client disconnected'));
 });
 
 setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
+    wss.clients.forEach((client) => {
+        client.send(new Date().toTimeString());
+    });
 }, 1000);
