7a1394469f9dd440b6f628aa27ed8d5e4d9cc334
Kike-Ramirez <info@kikeramirez.org>
2020-05-22 20:35:46 +0200

    PORT fixed

--- server.js
+++ server.js
@@ -3,8 +3,7 @@
 const express = require('express');
 const { Server } = require('ws');
 
-//const PORT = process.env.PORT || 5000;
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 const INDEX = '/index2.html';
 
 console.log(PORT);
